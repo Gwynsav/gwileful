10c49a8ddce3ac20ced38ff484b4284060c8173b
Gwynsav <n0n3ofyourbus1nes5@outlook.com>
2025-06-15 11:52:28 +0200

    Fix crashing on connection loss

--- signal/system/network/provider.lua
+++ signal/system/network/provider.lua
@@ -9,6 +9,7 @@ local gears = require('gears')
 
 local lgi  = require('lgi')
 local dbus = require('module.dbus_proxy')
+local helpers = require('helpers')
 
 local obj = gears.object({})
 
@@ -42,7 +43,9 @@ proxy.dev = {
 
 -- Auxiliary functions
 ----------------------
-local function get_proxy(iface, path)
+local _p = {}
+
+function _p.get_proxy(iface, path)
    return dbus.Proxy:new({
       bus = dbus.Bus.SYSTEM,
       name = 'org.freedesktop.NetworkManager',
@@ -52,36 +55,38 @@ local function get_proxy(iface, path)
 end
 
 -- Main daemon proxy.
-proxy.nm = get_proxy('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager')
+proxy.nm = _p.get_proxy('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager')
 
 -- Returns an icon name and network name for the default device (`proxy.dev.default`).
 -- If the connection is wireless, the icon is representative of signal strength and the
 -- name is that of the network SSID.
 -- If the connection is wired, the name is the interface's.
-local function get_default_information()
+function _p.get_default_information()
    local data = {}
 
+   -- No connection.
    if proxy.dev.default == nil then
       data.name = 'No connection'
       data.icon = 'none'
       return data
    end
 
-   local state = connectivity[proxy.nm:CheckConnectivity()]
-
+   -- Connected but unable to access the internet.
    local conn = true
+   local state = connectivity[proxy.nm:CheckConnectivity()]
    if state == 'UNKNOWN' or state == 'NONE' then
       conn = false
    end
 
    -- WiFi devices get a signal strength related icon and SSID.
    if proxy.dev.default.generic.DeviceType == dev_type.WIFI then
-      local ap_proxy = get_proxy('org.freedesktop.NetworkManager.AccessPoint',
-                                 proxy.dev.default.typed.ActiveAccessPoint)
-      data.name = nm.utils_ssid_to_utf8(ap_proxy.Ssid)
-      if not conn then
+      local ap_proxy = _p.get_proxy('org.freedesktop.NetworkManager.AccessPoint',
+                                    proxy.dev.default.typed.ActiveAccessPoint)
+      if not conn or ap_proxy.Ssid == nil then
+         data.name = 'No connection'
          data.icon = 'wifi_none'
       else
+         data.name = nm.utils_ssid_to_utf8(ap_proxy.Ssid)
          if ap_proxy.Strength > 66 then
             data.icon = 'wifi_high'
          elseif ap_proxy.Strength > 33 then
@@ -105,53 +110,90 @@ end
 
 -- Gets all active devices and classifies them into wired or wireless. Ignores symbolic
 -- interfaces such as loopback. Also binds the default device to `proxy.dev.default`.
-local function get_devices()
+function _p.get_devices()
+   proxy.dev.wireless = helpers.clear_array(proxy.dev.wireless)
+   proxy.dev.wired    = helpers.clear_array(proxy.dev.wired)
+   proxy.dev.default  = nil
+
    local _dev = proxy.nm:GetDevices()
    for _, path in ipairs(_dev) do
-      local dev_proxy = get_proxy('org.freedesktop.NetworkManager.Device', path)
+      local dev_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device', path)
 
       -- Divide devices into wired or wireless.
       local typed_proxy
       if dev_proxy.DeviceType == dev_type.WIFI then
-         typed_proxy = get_proxy('org.freedesktop.NetworkManager.Device.Wireless', path)
+         typed_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device.Wireless', path)
          table.insert({ generic = dev_proxy, typed = typed_proxy }, proxy.dev.wireless)
       elseif dev_proxy.DeviceType == dev_type.ETHERNET then
-         typed_proxy = get_proxy('org.freedesktop.NetworkManager.Device.Wired', path)
+         typed_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device.Wired', path)
          table.insert({ generic = dev_proxy, typed = typed_proxy }, proxy.dev.wired)
       end
 
       -- Add an extra entry for the current default device.
       if dev_proxy.State == dev_active then
-         local active_conn = get_proxy('org.freedesktop.NetworkManager.Connection.Active',
-                                       dev_proxy.ActiveConnection)
+         local active_conn = _p.get_proxy('org.freedesktop.NetworkManager.Connection.Active',
+                                          dev_proxy.ActiveConnection)
          if active_conn.Default then
+            -- if proxy.dev.default ~= nil then
+            --    proxy.dev.default.generic:disconnect_signal('StateChanged')
+            -- end
             proxy.dev.default = { generic = dev_proxy, typed = typed_proxy }
+            -- proxy.dev.default.generic:connect_signal(function()
+            --    _p.handle_device_change()
+            -- end, 'StateChanged')
          end
       end
    end
+end
+
+function _p.get_default()
+   local _dev = proxy.nm:GetDevices()
+   for _, path in ipairs(_dev) do
+      local dev_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device', path)
+      if dev_proxy.State == dev_active then
+         local active_conn = _p.get_proxy('org.freedesktop.NetworkManager.Connection.Active',
+                                          dev_proxy.ActiveConnection)
+         if active_conn.Default then
+            local typed_proxy
+            if dev_proxy.DeviceType == dev_type.WIFI then
+               typed_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device.Wireless', path)
+            elseif dev_proxy.DeviceType == dev_type.ETHERNET then
+               typed_proxy = _p.get_proxy('org.freedesktop.NetworkManager.Device.Wired', path)
+            end
+            proxy.dev.default = { generic = dev_proxy, typed = typed_proxy }
+            return
+         end
+      end
+   end
+   proxy.dev.default = nil
+end
+
+function _p.handle_device_change()
+   _p.get_devices()
+   obj:emit_signal('default_change', _p.get_default_information())
 end
 
 
 -- Global methods
 -----------------
 function obj:request_data()
-   obj:emit_signal('default_change', get_default_information())
+   obj:emit_signal('default_change', _p.get_default_information())
 end
 
 
 -- Signals
 ----------
 -- Do an initial run of the devices.
-get_devices()
-obj:emit_signal('default_change', get_default_information())
+_p.get_devices()
+obj:emit_signal('default_change', _p.get_default_information())
 
--- Of course it wouldn't be that easy, can't have documentation being accurate, can we?
-proxy.dev.default.generic:connect_signal(function()
-   obj:emit_signal('default_change', get_default_information())
+proxy.nm:connect_signal(function()
+   _p.get_default()
+   obj:emit_signal('default_change', _p.get_default_information())
 end, 'StateChanged')
 
--- proxy.nm:connect_signal('DeviceAdded', function()
---    print('hi')
--- end)
+-- On a new device being added or a current one being removed.
+proxy.nm:connect_signal(_p.handle_device_change(), 'DeviceAdded')
+proxy.nm:connect_signal(_p.handle_device_change(), 'DeviceRemoved')
 
 return obj
